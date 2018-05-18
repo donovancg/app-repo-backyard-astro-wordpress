4890799e731315df533d87cfd37e0587ed82aaa0
Donovan Campbell-Gillies <dcgillies@Franks-Mac-mini.frontierlocal.net>
2018-05-18 13:44:06 -0700

    added files

--- wp-content/themes/nighttimedark/index.php
+++ wp-content/themes/nighttimedark/index.php
@@ -50,4 +50,27 @@
         </section>
     </main>
 
+    <?php
+    
+    
+    $args = array(
+        'numberposts' => 1,
+        'offset' => 0,
+        'category' => 0,
+        'orderby' => 'post_date',
+        'order' => 'DESC',
+        'include' => '',
+        'exclude' => '',
+        'meta_key' => '',
+        'meta_value' =>'',
+        'post_type' => 'post',
+        'post_status' => 'draft, publish, future, pending, private',
+        'suppress_filters' => true
+    );
+
+    $recent_posts = wp_get_recent_posts( $args, ARRAY_A );
+    print_r($recent_posts);
+?>
+    
+
 <?php get_footer(); ?>
